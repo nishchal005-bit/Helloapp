1ac7214b85dd1acaa74dd75e6e4786a2beb08dad
nishchal005-bit <nishchal005@gmail.com>
2026-04-21 23:01:46 +0530

    uc-3

--- uc-3/helloApp.java
+++ uc-3/helloApp.java
@@ -1,18 +1,6 @@
 public class HelloApp {
     public static void main(String[] args) {
-
-        String greeting;
-
-        // Check if no arguments are provided
-        if (args.length == 0) {
-            greeting = "Hello, World!";
-        } else {
-            // Join all names with ", "
-            String names = String.join(", ", args);
-            greeting = "Hello, " + names + "!";
-        }
-
-        // Print greeting
-        System.out.println(greeting);
+        String name = (args.length > 0) ? args[0] : "World";
+        System.out.println("Hello, " + name + "!");
     }
 }
